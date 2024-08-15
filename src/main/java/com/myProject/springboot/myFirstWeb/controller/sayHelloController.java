515ed6189d6b93a4ea04b6eab42137413366f5dc
Ankit-sinhaha <ankitecesinha@gmail.com>
2024-08-15 16:17:28 +0530

    first rest controller

---
+++ src/main/java/com/myProject/springboot/myFirstWeb/controller/sayHelloController.java
@@ -0,0 +1,13 @@
+package com.myProject.springboot.myFirstWeb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class sayHelloController {
+
+    @RequestMapping("say-hello")
+    public String sayHello() {
+        return "Hello World";
+    }
+}
