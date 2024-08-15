7ab2ee094cd59d04af74bac47e209daf38ea27e7
Ankit-sinhaha <ankitecesinha@gmail.com>
2024-08-15 18:51:47 +0530

    view in spring boot

--- src/main/java/com/myProject/springboot/myFirstWeb/controller/sayHelloController.java
+++ src/main/java/com/myProject/springboot/myFirstWeb/controller/sayHelloController.java
@@ -1,23 +1,26 @@
 package com.myProject.springboot.myFirstWeb.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class sayHelloController {
 
-    @RequestMapping("say-hello-html")
-    public String sayHello() {
-        StringBuffer sb = new StringBuffer();
-        sb.append("<html>");
-        sb.append("<head>");
-        sb.append("<title> My first HTML page </title>");
-        sb.append("</head>");
-        sb.append("<body>");
-        sb.append("Hello! how are you?");
-        sb.append("</body>");
-        sb.append("</html>");
 
-        return sb.toString();
+    // src/main/resources/META-INF/resources/WEB-INF/jsp
+    @RequestMapping("say-hello-jsp")
+    public String sayHelloJsp() {
+        return "sayHello";
     }
+
+    // we are sending a request to say-hello-jsp
+    // this request is being handled by sayHelloController
+    // sayHellojsp method is mapped to this URL
+    // this method is handling the request and returning back the name of the jsp file
+    // spring mvc is making use of view resolver to resolve the view
+    // we are configuring the view resolver in the application.properties file
+    // we are configuring the prefix and suffix of the view resolver
+
 }
