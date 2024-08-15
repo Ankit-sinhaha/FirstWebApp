246a4fea76be8fd2f5d6ac5a1e3cf4fce5b02089
Ankit-sinhaha <ankitecesinha@gmail.com>
2024-08-15 16:30:19 +0530

    first html with spring boot

--- src/main/java/com/myProject/springboot/myFirstWeb/controller/sayHelloController.java
+++ src/main/java/com/myProject/springboot/myFirstWeb/controller/sayHelloController.java
@@ -6,8 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class sayHelloController {
 
-    @RequestMapping("say-hello")
+    @RequestMapping("say-hello-html")
     public String sayHello() {
-        return "Hello World";
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title> My first HTML page </title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("Hello! how are you?");
+        sb.append("</body>");
+        sb.append("</html>");
+
+        return sb.toString();
     }
 }
