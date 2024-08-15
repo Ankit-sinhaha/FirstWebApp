20da2f2ba32e7e763e718597a629f30769fc7ecf
Ankit-sinhaha <ankitecesinha@gmail.com>
2024-08-15 20:59:57 +0530

    logging in spring boot

--- src/main/java/com/myProject/springboot/myFirstWeb/login/LogInController.java
+++ src/main/java/com/myProject/springboot/myFirstWeb/login/LogInController.java
@@ -1,6 +1,8 @@
 package com.myProject.springboot.myFirstWeb.login;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LogInController {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+
     // Query parameters
     // http://localhost:8080/login?name=ankit
     //  to pass name from user to our jsp we use model map
@@ -16,6 +21,8 @@ public class LogInController {
     public String goToLoginPage(@RequestParam String name, ModelMap modelMap) {
         modelMap.put("name", name); // (key, value ) pair
         System.out.println("name is: " + name); // not recommended for production code
+        logger.debug("Request param at Debug Level is {} " + name);
+        logger.info("Request param  at Info Level is {} " + name);
         return "login";
     }
 
